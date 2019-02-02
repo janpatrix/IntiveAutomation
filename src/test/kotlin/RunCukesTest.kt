7e9aee6808d8e92e4cce2c6e4f57a083d58fd56f
janpatrix <janpatrix@gmail.com>
2019-02-02 10:10:34 +0100

    Better reporting

--- src/test/kotlin/RunCukesTest.kt
+++ src/test/kotlin/RunCukesTest.kt
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith
 @CucumberOptions(
     plugin = ["pretty"],
     features = ["src/test/resources/features/"],
+    format = ["json:target/cucumber.json"],
     tags = ["@amazon"])
 
 open class RunCukesTest
